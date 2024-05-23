5f8a98f15be3183d1cc4e3c50ea4061cc5e000bf
Juliapeixerr <juliabduartep14@gmail.com>
2024-05-23 19:59:03 -0300

    ✔Adicionando classes faltantes

---
+++ Loja01/Project/Database/Finder/GenericClienteFinder.cs
@@ -0,0 +1,19 @@
+﻿using Loja01.Project.Domain.Models;
+using System.Linq.Expressions;
+
+namespace Loja01.Project.Database.Finder
+{
+    public class GenericClienteFinder
+    {
+        private string? _nome;
+
+        public GenericClienteFinder Nome(string? nome)
+        {
+            _nome = nome;
+            return this;
+        }
+
+        public Expression<Func<Cliente, bool>> ToExpression()
+            => (cliente) => (_nome == null || cliente.Nome == _nome);
+    }
+}
---
+++ Loja01/Project/Domain/Command/LoginClienteCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Loja01.Project.Domain.Command
+{
+    public class LoginClienteCommand
+    {
+        public string? Senha { get; set; }
+        public string? Usuario { get; set; }
+    }
+}
